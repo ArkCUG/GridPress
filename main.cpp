ddf5bff35567feed56b87d32104b87bf2661b085
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-03-12 12:49:27 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   main.cpp 	new file:   source.txt

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main(){
+    ifstream ifs("source.txt");
+    if(!ifs){
+        cout<<"No such file named source.txt"<<endl;
+        return -1;
+    }
+    string s;
+    while(getline(ifs,s)){
+        cout<<s<<endl;
+    }
+    return 0;
+}
