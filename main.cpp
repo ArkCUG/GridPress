1c175831b1218971577b2b60593e037b65076517
LaTristan <50165836+LaTristan@users.noreply.github.com>
2021-03-14 14:18:05 +0800

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+bool checkRect(vector<vector<int>>& src,int i,int j,int l){
+    int r=i+l;
+    int c=j+l;
+    if(src[r][c]!=src[i][j])return false;
+    
+}
+
+int detectLength(vector<vector<int>>& src,int i,int j){
+    int val=src[i][j];
+    for(int l=1;l<src.size()-i&&l<src.size()-j;l++){
+
+        l++;
+    }
+}
+
 int main(){
     ifstream ifs("source.txt");
     if(!ifs){
